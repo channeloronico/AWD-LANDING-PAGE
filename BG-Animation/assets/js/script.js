d626405f1b970ce0710bb4b0e4f22acaa28291f3
channeloronico <155803043+channeloronico@users.noreply.github.com>
2024-03-23 14:53:34 +0800

    Update script.js

--- BG-Animation/assets/js/script.js
+++ BG-Animation/assets/js/script.js
@@ -1,1 +1,5 @@
-
+for (let index = 1; index < 2000; index++) {
+	const element = document.createElement('span');
+	element.classList.add('pixel');
+	document.getElementById('_container').appendChild(element);
+}
